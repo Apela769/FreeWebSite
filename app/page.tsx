988c5cab6c64d3706e55b425c0a2e1e17f301327
𝗔𝗽𝗲𝗹𝗮.𝗘𝘅𝗲! <benlahcenabderrahmane773@gmail.com>
2026-05-28 14:09:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -51,16 +51,16 @@ export default function ProfilePage() {
 
             {/* Spotify Player */}
             <div className="mb-8">
-  <iframe
-    style={{ borderRadius: '12px' }}
-    src="https://open.spotify.com/embed/playlist/3d0fhtCuf3o7bXQx4ri5S1?utm_source=generator"
-    width="100%"
-    height="380"
-    frameBorder="0"
-    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-    loading="lazy"
-  />
-</div>
+             <iframe
+              style={{ borderRadius: '12px' }}
+              src="https://open.spotify.com/embed/playlist/3d0fhtCuf3o7bXQx4ri5S1?utm_source=generator"
+              width="100%"
+              height="152"
+              frameBorder="0"
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+            />
+          </div>
 
 
 
